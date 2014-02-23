b7153e9cc14b3e041df5ef144d726a970eb6232c
Matthew Frost <mf@matthewfrost.com>
2014-02-23 11:41:48 -0500

    use clojure v1.5.1

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject followersdb "1.0"
   :description "Store followers of a Twitter account"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])

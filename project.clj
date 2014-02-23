4756cf9d4a90555cd1dc33e19b766139f8470bcc
Matthew Frost <mf@matthewfrost.com>
2014-02-23 13:12:44 -0500

    get follower ids from API

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject followersdb "1.0"
+(defproject followersdb "0.1"
   :description "Store followers of a Twitter account"
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [twitter-api "0.7.5"]])
--- src/followersdb/updatefollowers.clj
+++ src/followersdb/updatefollowers.clj
@@ -1,7 +1,26 @@
-(ns followersdb.updatefollowers)
+(ns followersdb.updatefollowers
+  (:use
+   [twitter.oauth]
+   [twitter.api.restful]))
+
+(declare update)
 
 (defn -main [& args]
   (if (nil? args)
-    (do (println "No handle specified!"))
-    (do
-    (println (str "Updating followers of " (first args))))))
+    (println "No handle specified!")
+    (update (first args))))
+
+(def oauth-creds
+  (make-oauth-creds (System/getenv "TWITTER_CONSUMER_KEY")
+                    (System/getenv "TWITTER_CONSUMER_SECRET")
+                    (System/getenv "TWITTER_ACCESS_KEY")
+                    (System/getenv "TWITTER_ACCESS_SECRET")))
+
+(defn update [handle]
+  (println (str "Updating followers of " handle))
+  (def ids (followers-ids :oauth-creds oauth-creds
+                    :params {:screen-name handle}))
+  (println ids) ;DELME
+  ;TODO lookup ids via users-lookup
+  ;TODO store in db
+  (println (str "Updating followers of " handle " complete!")))

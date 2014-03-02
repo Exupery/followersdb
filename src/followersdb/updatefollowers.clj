f4e7a5253dcb223c18f6a6e379fe0c8270b28c5a
Matthew Frost <mf@matthewfrost.com>
2014-03-02 13:51:45 -0500

    lookup full user objects for follower ids

--- src/followersdb/updatefollowers.clj
+++ src/followersdb/updatefollowers.clj
@@ -16,11 +16,22 @@
                     (System/getenv "TWITTER_ACCESS_KEY")
                     (System/getenv "TWITTER_ACCESS_SECRET")))
 
+(defn lookup-users [user-ids]
+  (:body (users-lookup :oauth-creds oauth-creds
+                :params {:user-id (clojure.string/join "," user-ids)})))
+
 (defn update [handle]
   (println (str "Updating followers of " handle))
   (def ids (followers-ids :oauth-creds oauth-creds
-                    :params {:screen-name handle}))
-  (println ids) ;DELME
-  ;TODO lookup ids via users-lookup
-  ;TODO store in db
+                          :params {:screen-name handle}))
+  (if (= (:code (:status ids)) 200)
+    (do
+      (def followers (flatten (map lookup-users (partition-all 100 (:ids (:body ids))))))
+      (println (first followers)) ;DELME
+      (println (count followers)) ;DELME
+      ;TODO store in db
+     )
+    (println (str
+              "Unable to retrieve follower IDs, code: "
+              (:code (:status ids)) " msg: " (:msg (:status ids)))))
   (println (str "Updating followers of " handle " complete!")))
